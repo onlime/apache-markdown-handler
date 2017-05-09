c922a0364aa0de82a48b0163574d4a6f76421955
Philip Iezzi <github@onlime.ch>
2017-05-10 00:30:06 +0200

    removed thirdparty assets and replaced by Bower packages added recommended HTML5 meta tags

--- handler.php
+++ handler.php
@@ -1,38 +1,39 @@
 <?php
-require_once(dirname(__FILE__).'/vendor/autoload.php');
+// Composer autoloading
+require_once dirname(__FILE__) . '/vendor/autoload.php';
 
 header('Content-type: text/html; charset=utf-8');
 
 define('DOCUMENT_ROOT', str_replace('\\', '/', $_SERVER['DOCUMENT_ROOT']));
-define('FOLDER_ROOT', str_replace(DOCUMENT_ROOT, '', str_replace('\\', '/', dirname(__FILE__))));
-$allowedExtensions=array('md', 'markdown');
+define('FOLDER_ROOT', '/apache-markdown-handler');
+$allowedExtensions = ['md', 'markdown'];
 ?>
 <!DOCTYPE html>
 <html>
-    <head>
-        <meta name="content-type" http-equiv="content-type" value="text/html; utf-8"/>
-        
-        <link rel="stylesheet" type="text/css" href="<?php print FOLDER_ROOT; ?>/css/normalize.css"/>
-        <link rel="stylesheet" type="text/css" href="<?php print FOLDER_ROOT; ?>/css/utilities.css"/>
-        <link rel="stylesheet" type="text/css" href="<?php print FOLDER_ROOT; ?>/css/typography.css"/>
-        <link rel="stylesheet" type="text/css" href="<?php print FOLDER_ROOT; ?>/css/layout.css"/>
-        <link rel="stylesheet" type="text/css" href="<?php print FOLDER_ROOT; ?>/css/small.css"/>
-    </head>
-    <body>
-        <div class="wrapper">
-            <?php
-            $file=str_replace('\\', '/', realpath($_SERVER['PATH_TRANSLATED']));
-            if($file && is_file($file) && in_array(strtolower(substr($file, strrpos($file, '.')+1)), $allowedExtensions) && substr($file, 0, strlen(DOCUMENT_ROOT))==DOCUMENT_ROOT) {
-                $parser=new ParsedownExtra();
-                print $parser->Parse(file_get_contents($file));
-            }else {
-                print '<p>Bad filename given</p>';
-            }
-            ?>
-        </div>
-        
-        <script type="text/javascript" src="https://cdn.rawgit.com/google/code-prettify/master/loader/run_prettify.js"></script>
-        <script type="text/javascript" src="<?php print FOLDER_ROOT; ?>/thirdparty/jquery/jquery.min.js"></script>
-        <script type="text/javascript" src="<?php print FOLDER_ROOT; ?>/javascript/main.js"></script>
-    </body>
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" href="<?= FOLDER_ROOT ?>/bower_components/normalize-css/normalize.css">
+    <link rel="stylesheet" type="text/css" href="<?= FOLDER_ROOT ?>/css/utilities.css">
+    <link rel="stylesheet" type="text/css" href="<?= FOLDER_ROOT ?>/css/typography.css">
+    <link rel="stylesheet" type="text/css" href="<?= FOLDER_ROOT ?>/css/layout.css">
+    <link rel="stylesheet" type="text/css" href="<?= FOLDER_ROOT ?>/css/small.css">
+</head>
+<body>
+<div class="wrapper">
+    <?php
+    $file = str_replace('\\', '/', realpath($_SERVER['PATH_TRANSLATED']));
+    if ($file && is_file($file) && in_array(strtolower(substr($file, strrpos($file, '.') + 1)), $allowedExtensions) && substr($file, 0, strlen(DOCUMENT_ROOT)) == DOCUMENT_ROOT) {
+        $parser = new ParsedownExtra();
+        print $parser->Parse(file_get_contents($file));
+    } else {
+        print '<p>Bad filename given</p>';
+    }
+    ?>
+</div>
+<script type="text/javascript" src="https://cdn.rawgit.com/google/code-prettify/master/loader/run_prettify.js"></script>
+<script type="text/javascript" src="<?= FOLDER_ROOT ?>/bower_components/jquery/dist/jquery.min.js"></script>
+<script type="text/javascript" src="<?= FOLDER_ROOT ?>/javascript/main.js"></script>
+</body>
 </html>
